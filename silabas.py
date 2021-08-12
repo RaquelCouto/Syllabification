1d8defd260d1166a2e13fc46ab70751aa59e4762
RaquelCouto <raquelcouto@outlook.com>
2021-08-12 00:27:35 -0300

    Um monte de mudança
    agora ele confere mais palavras truncadas e atende a espanhol

--- silabas.py
+++ silabas.py
@@ -11,7 +11,7 @@ divisaoSilabica = ''
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-image = cv2.imread('spotify-correto.png')
+image = cv2.imread('russo.png')
 
 img_grayScale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(img_grayScale, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
@@ -20,7 +20,7 @@ binimagem = Image.fromarray(thresh)
 
 #print(pytesseract.image_to_string(binimagem, lang='por'))
 
-textoImg = pytesseract.image_to_string(binimagem, lang='por')
+textoImg = pytesseract.image_to_string(binimagem, lang='rus')
 
 #convertendo a string retornada em uma lista de palavras
 print(textoImg)
@@ -28,7 +28,7 @@ textoImag = textoImg.replace('\n',' ')
 #print(textoImag)
 #textoImag = textoImg.strip('\n')
 ListofWords = list(textoImag.split(" "))
-print(ListofWords)
+#print(ListofWords)
 
 # Removing from the list all elements that are not strings
 
@@ -58,9 +58,9 @@ for i in range(0,(len(filteredList)-1)):
 print(palavrasConcatenadas)
 
 #aqui eu crio um dicionário com as palavras e divisão silábica
-with open('palavras.csv', newline='') as csvfile:
+with open('russo.csv', encoding="utf8",newline='') as csvfile:
     dicionario = csv.reader(csvfile)#, quotechar='|')
-
+    print("O dicionario aqui ó:::", dicionario)
     for linha in dicionario:
         for elemento in linha:
             elemento = elemento.split(';')
@@ -71,43 +71,47 @@ with open('palavras.csv', newline='') as csvfile:
 #    print(silabasDict)
 
 palavra = []
+divisaoSilabica = []
 
 for elemento in palavrasConcatenadas:
     if elemento[0] in silabasDict:
-        palavra = elemento
-        divisaoSilabica = silabasDict[elemento[0]]
-        print(palavra)
-        print(divisaoSilabica);
-        break
+        palavra.append(elemento)
+        divisaoSilabica.append(silabasDict[elemento[0]])
+        #print(palavra)
 
-if divisaoSilabica == '':
-    print('palavra não encontrada')
+print(divisaoSilabica)
+
+if len(divisaoSilabica) == 0:
+    print('nenhuma palavra encontrada')
 
 #operações para somar as silabas
 
 silabas = []
-silabasConcatenadas = []
-silabas = divisaoSilabica.split('-')
+
+for elemento in divisaoSilabica:
+    silabas.append(elemento.split('-'))
+
 print(silabas)
 
 
 
 quebrasAceitaveis = []
-aux = ''
 
-for i in range(0,len(silabas)-1):
-    variavel = silabas[i]
-    aux = aux + variavel
-    quebrasAceitaveis.append(aux)
 
-#print(quebrasAceitaveis)
-aux = ''
-
-for i in range(len(silabas)-1,0,-1):
-    variavel = silabas[i]
-    aux = variavel + aux
-    quebrasAceitaveis.append(aux)
-
+for silaba in silabas:
+    aux = ''
+    quebras = []
+    for i in range(0,len(silaba)-1):
+        variavel = silaba[i]
+        aux = aux + variavel
+        quebras.append(aux)
+    aux = ''
+    for i in range(len(silaba)-1,0,-1):
+        variavel = silaba[i]
+        aux = variavel + aux
+        quebras.append(aux)
+    quebrasAceitaveis.append(quebras)
+print(palavra)
 print(quebrasAceitaveis)
 
 # função para checar se uma lista está contida em outra
@@ -121,7 +125,14 @@ def list_contains(List1, List2):
 
 # checagem final
 
-if list_contains(palavra,quebrasAceitaveis):
-    print("A palavra {} foi separadas em silábas corretamente".format(palavra[0]))
-else:
-    print("A palavra {} foi separadas em silábas erroneamente".format(palavra[0]))
+for p in palavra:
+    flag = False
+    for quebra in quebrasAceitaveis:
+        if list_contains(p, quebra):
+            flag = True
+            break
+    if flag:
+        print("A palavra '{}' foi separadas em sílabas corretamente: {}-{}".format(p[0],p[1],p[2]))
+    else:
+        print("A palavra '{}' foi separadas em sílabas erroneamente: {}-{}".format(p[0],p[1],p[2]))
+
